79c3bcf505ac112b7f9f565fd3101ba8b9f7640e
JW <jacekwoln@gmail.com>
2022-12-04 20:11:49 +0100

    banner

--- src/main/java/jw/instruments/spigot/PluginDocumentation.java
+++ src/main/java/jw/instruments/spigot/PluginDocumentation.java
@@ -2,6 +2,7 @@ package jw.instruments.spigot;
 
 import jw.fluent.api.spigot.documentation.api.DocumentationDecorator;
 import jw.fluent.api.spigot.documentation.api.models.Documentation;
+import jw.fluent.api.spigot.messages.message.MessageBuilder;
 import jw.fluent.api.utilites.java.StringUtils;
 import jw.instruments.core.data.Consts;
 
@@ -10,16 +11,17 @@ public class PluginDocumentation extends DocumentationDecorator {
     public void decorate(Documentation documentation) {
 
 
-        addText("Thank you for using JW Instruments don't forget to leave review ", documentation,"spiogt-ignore","github-ignore");
-        addText("Those links might be helpful in case you need any help", documentation,"spiogt-ignore","github-ignore");
-        addText(StringUtils.EMPTY_STRING, documentation,"spiogt-ignore","github-ignore");
-        addText("  Spigot: "+ Consts.SPIGOT_URL, documentation,"spiogt-ignore","github-ignore");
-        addText("  Discord: "+ Consts.DISCORD_URL, documentation,"spiogt-ignore","github-ignore");
-        addText("  Github: "+ Consts.GITHUB_URL, documentation,"spiogt-ignore","github-ignore");
-        addText(StringUtils.EMPTY_STRING, documentation,"spiogt-ignore","github-ignore");
-        addText("!Be aware this is only documentation, changes in this file has not impact on plugin", documentation,"spiogt-ignore","github-ignore");
+        addText("Thank you for using JW Instruments don't forget to leave review ", documentation, "spigot-ignore", "github-ignore");
+        addText("Those links might be helpful in case you need any help", documentation, "spigot-ignore", "github-ignore");
+        addText(StringUtils.EMPTY_STRING, documentation, "spigot-ignore", "github-ignore");
+        addText("  Spigot: " + Consts.SPIGOT_URL, documentation, "spigot-ignore", "github-ignore");
+        addText("  Discord: " + Consts.DISCORD_URL, documentation, "spigot-ignore", "github-ignore");
+        addText("  Github: " + Consts.GITHUB_URL, documentation, "spigot-ignore", "github-ignore");
+        addText(StringUtils.EMPTY_STRING, documentation, "spigot-ignore", "github-ignore");
+        addText("!Be aware this is only documentation, changes in this file has not impact on plugin", documentation, "spigot-ignore", "github-ignore");
 
         addImage("https://raw.githubusercontent.com/jwdeveloper/JW_Instruments/master/resources/banner_plugin.jpg", documentation);
+        addText(banner(),documentation);
 
         var builder = createStringBuilder();
         builder.newLine()
@@ -28,15 +30,31 @@ public class PluginDocumentation extends DocumentationDecorator {
                 .text("Therefore consider to use this plugin especially on RolePlay server")
                 .newLine();
 
-        addText(builder.build(), documentation,"plugin-ignore");
+        addText(builder.build(), documentation, "plugin-ignore");
 
-        addText("Plugin tutorial: https://www.youtube.com/watch?v=F4iKXAMIioo&ab_channel=JW", documentation,"spigot-ignore");
+        addText("Plugin tutorial: https://www.youtube.com/watch?v=F4iKXAMIioo&ab_channel=JW", documentation, "spigot-ignore");
         addVideo("https://www.youtube.com/watch?v=F4iKXAMIioo&ab_channel=JW", documentation);
-
 
 
         addImage("https://raw.githubusercontent.com/jwdeveloper/JW_Instruments/master/resources/guitars.gif", documentation);
         addImage("https://raw.githubusercontent.com/jwdeveloper/JW_Instruments/master/resources/chords.gif", documentation);
         addImage("https://raw.githubusercontent.com/jwdeveloper/JW_Instruments/master/resources/chordchange.gif", documentation);
     }
+
+
+    public String banner() {
+        var builder = createStringBuilder();
+        bannerElement(builder, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgmEAfLQGWzAbZnqlmp7mCIfDyMx7hovnhLw&usqp=CAU", Consts.DISCORD_URL);
+        bannerElement(builder, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgmEAfLQGWzAbZnqlmp7mCIfDyMx7hovnhLw&usqp=CAU", Consts.GITHUB_URL);
+        bannerElement(builder, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgmEAfLQGWzAbZnqlmp7mCIfDyMx7hovnhLw&usqp=CAU", Consts.SPIGOT_URL);
+        return builder.build();
+    }
+
+    public void bannerElement(MessageBuilder builder, String image, String link) {
+        builder.text(" [URL='" + link + "']");
+        builder.text("[IMG]").text(image).text("[/IMG]");
+        builder.text("[/URL]");
+    }
+
+
 }
